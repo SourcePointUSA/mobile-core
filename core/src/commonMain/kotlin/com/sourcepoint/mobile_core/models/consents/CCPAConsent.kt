aa8d468c8166ec8e29c199f52885466ae7c4ecb4
André Herculano <andresilveirah@gmail.com>
2024-09-09 11:13:10 +0200

    implement CCPAConsent class

---
+++ core/src/commonMain/kotlin/com/sourcepoint/mobile_core/models/consents/CCPAConsent.kt
@@ -0,0 +1,30 @@
+package com.sourcepoint.mobile_core.models.consents
+
+import com.sourcepoint.mobile_core.utils.StringEnumWithDefaultSerializer
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.json.JsonObject
+
+@Serializable
+data class CCPAConsent(
+    val uuid: String? = null,
+    val dateCreated: String? = null,
+    val expirationDate: String? = null,
+    val signedLspa: Boolean? = null,
+    var uspstring: String? = null,
+    val rejectedVendors: List<String>? = emptyList(),
+    val rejectedCategories: List<String>? = emptyList(),
+    val status: CCPAConsentStatus = CCPAConsentStatus.Unknown,
+    @SerialName("GPPData") val gppData: JsonObject = JsonObject(emptyMap()),
+) {
+    @Serializable(with = CCPAConsentStatus.Serializer::class)
+    enum class CCPAConsentStatus {
+        @SerialName("consentedAll") ConsentedAll,
+        @SerialName("rejectedAll") RejectedAll,
+        @SerialName("rejectedSome") RejectedSome,
+        @SerialName("rejectedNone") RejectedNone,
+        @SerialName("linkedNoAction") LinkedNoAction,
+        Unknown;
+        object Serializer: StringEnumWithDefaultSerializer<CCPAConsentStatus>(entries, Unknown)
+    }
+}
