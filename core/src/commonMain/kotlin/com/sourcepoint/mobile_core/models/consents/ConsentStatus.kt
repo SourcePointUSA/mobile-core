c832447a3c06cf88e9f0b8a2de24a17b53b0261e
André Herculano <andresilveirah@gmail.com>
2024-09-12 13:50:56 +0200

    add missing properties on ConsentStatus

--- core/src/commonMain/kotlin/com/sourcepoint/mobile_core/models/consents/ConsentStatus.kt
+++ core/src/commonMain/kotlin/com/sourcepoint/mobile_core/models/consents/ConsentStatus.kt
@@ -6,9 +6,13 @@ import kotlinx.serialization.Serializable
 data class ConsentStatus (
     val rejectedAny: Boolean? = null,
     val rejectedLI: Boolean? = null,
+    val rejectedAll: Boolean? = null,
     val consentedAll: Boolean? = null,
-    val hasConsentData: Boolean? = null,
+    val consentedToAll: Boolean? = null,
     val consentedToAny: Boolean? = null,
+    val hasConsentData: Boolean? = null,
+    val vendorListAdditions: Boolean? = null,
+    val legalBasisChanges: Boolean? = null,
     val granularStatus: ConsentStatusGranularStatus? = null
 ) {
     @Serializable
@@ -18,6 +22,10 @@ data class ConsentStatus (
         val purposeConsent: String? = null,
         val purposeLegInt: String? = null,
         val previousOptInAll: Boolean? = null,
-        val defaultConsent: Boolean? = null
+        val defaultConsent: Boolean? = null,
+        val sellStatus: Boolean? = null,
+        val shareStatus: Boolean? = null,
+        val sensitiveDataStatus: Boolean? = null,
+        val gpcStatus: Boolean? = null
     )
 }
