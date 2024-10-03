aaafc502e55493c349e85d0d8a24e66f9f844d59
Dmytro <dmytro.f@gowombat.team>
2024-10-03 12:43:27 +0200

    Add `CustomConsentRequest` class

---
+++ core/src/commonMain/kotlin/com/sourcepoint/mobile_core/network/requests/CustomConsentRequest.kt
@@ -0,0 +1,12 @@
+package com.sourcepoint.mobile_core.network.requests
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class CustomConsentRequest (
+    val consentUUID: String,
+    val propertyId: Int,
+    val vendors: List<String> = emptyList(),
+    val categories: List<String> = emptyList(),
+    val legIntCategories: List<String> = emptyList()
+): DefaultRequest() {}
