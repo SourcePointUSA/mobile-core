17528c874bc064c5c0f9953ee9ee62243e880672
Dmytro <dmytro.f@gowombat.team>
2024-12-19 17:34:32 +0100

    Implement `postChoice`s

--- core/src/commonMain/kotlin/com/sourcepoint/mobile_core/Coordinator.kt
+++ core/src/commonMain/kotlin/com/sourcepoint/mobile_core/Coordinator.kt
@@ -25,6 +25,27 @@ interface SPCoordinator {
         action: SPAction,
         campaigns: ChoiceAllRequest.ChoiceAllCampaigns
     ): ChoiceAllResponse?
+
+    @Throws(Exception::class) suspend fun postChoiceGDPR(
+        idfaStatus:SPIDFAStatus,
+        includeData: IncludeData,
+        authId: String?,
+        postPayloadFromGetCall: ChoiceAllResponse.GDPR.PostPayload?,
+        action: SPAction
+    ): GDPRChoiceResponse
+
+    @Throws(Exception::class) suspend fun postChoiceCCPA(
+        includeData: IncludeData,
+        authId: String?,
+        action: SPAction
+    ): CCPAChoiceResponse
+
+    @Throws(Exception::class) suspend fun postChoiceUSNat(
+        includeData: IncludeData,
+        idfaStatus:SPIDFAStatus,
+        authId: String?,
+        action: SPAction
+    ): USNatChoiceResponse
 }
 class Coordinator(
     private val accountId: Int,
@@ -63,26 +84,134 @@ class Coordinator(
         return message
     }
 
-    private fun shouldCallGetChoice(action: SPActionType): Boolean =
-        (action == SPActionType.AcceptAll || action == SPActionType.RejectAll)
+    private fun handleGetChoiceAll(response: ChoiceAllResponse, campaign: SPCampaignType) {
+        if (response.gdpr != null && campaign == SPCampaignType.Gdpr) {
+            state.gdpr?.dateCreated = response.gdpr.dateCreated
+            state.gdpr?.expirationDate = response.gdpr.expirationDate
+            state.gdpr?.tcData = response.gdpr.tcData?: emptyMap()
+            state.gdpr?.grants = response.gdpr.grants
+            state.gdpr?.euconsent = response.gdpr.euconsent
+            state.gdpr?.consentStatus = response.gdpr.consentStatus
+            //state.gdpr?.childPmId = gdpr.childPmId
+            state.gdpr?.gcmStatus = response.gdpr.gcmStatus
+        }
+        if (response.ccpa != null && campaign == SPCampaignType.Ccpa) {
+            state.ccpa?.dateCreated = response.ccpa.dateCreated
+            state.ccpa?.expirationDate = response.ccpa.expirationDate
+            state.ccpa?.status = response.ccpa.status
+            state.ccpa?.gppData = response.ccpa.gppData
+        }
+        if (response.usnat != null && campaign == SPCampaignType.UsNat) {
+            state.usNat?.dateCreated = response.usnat.dateCreated
+            state.usNat?.expirationDate = response.usnat.expirationDate
+            state.usNat?.consentStatus = response.usnat.consentStatus
+            state.usNat?.gppData = response.usnat.gppData
+            state.usNat?.consentStrings = response.usnat.consentStrings
+            state.usNat?.consentStatus?.consentedToAll = response.usnat.consentedToAll
+            state.usNat?.consentStatus?.rejectedAny = response.usnat.rejectedAny
+            state.usNat?.consentStatus?.granularStatus?.gpcStatus = response.usnat.gpcEnabled
+        }
+    }
 
-    override suspend fun getChoiceAll(action: SPActionType, campaigns: ChoiceAllRequest.ChoiceAllCampaigns): ChoiceAllResponse? =
-        if (shouldCallGetChoice(action))
+    private fun shouldCallGetChoice(actionType: SPActionType): Boolean =
+        (actionType == SPActionType.AcceptAll || actionType == SPActionType.RejectAll)
+
+    override suspend fun getChoiceAll(action: SPAction, campaigns: ChoiceAllRequest.ChoiceAllCampaigns): ChoiceAllResponse? =
+        if (!shouldCallGetChoice(action.type))
+            null
+        else
             try {
-                spClient.getChoiceAll(action,campaigns)
+                val response = spClient.getChoiceAll(action.type,campaigns)
+                handleGetChoiceAll(response, action.campaignType)
+                response
             } catch (error: Throwable) {
                 throw error
             }
-        else
-            null
 
-    suspend fun postChoice(gdprChoiceRequest: GDPRChoiceRequest, action: SPActionType): GDPRChoiceResponse =
-        spClient.postChoiceGDPRAction(actionType = action, request = gdprChoiceRequest)
+    override suspend fun postChoiceGDPR(
+        idfaStatus:SPIDFAStatus,
+        includeData: IncludeData,
+        authId: String?,
+        postPayloadFromGetCall: ChoiceAllResponse.GDPR.PostPayload?,
+        action: SPAction
+    ): GDPRChoiceResponse =
+        try {
+            spClient.postChoiceGDPRAction(
+                actionType = action.type,
+                request = GDPRChoiceRequest(
+                    authId = authId,
+                    uuid = state.gdpr?.uuid,
+                    messageId = action.messageId,
+                    consentAllRef = postPayloadFromGetCall?.consentAllRef,
+                    vendorListId = postPayloadFromGetCall?.vendorListId,
+                    pubData = action.encodablePubData,
+                    pmSaveAndExitVariables = action.pmPayload,
+                    sendPVData = state.gdprMetaData?.wasSampled ?: false,
+                    propertyId = propertyId,
+                    sampleRate = state.gdprMetaData?.sampleRate,
+                    idfaStatus = idfaStatus,
+                    granularStatus = postPayloadFromGetCall?.granularStatus,
+                    includeData = includeData
+                )
+            )
+        }
+        catch (error: Throwable) {
+            throw error
+        }
 
-    suspend fun postChoice(ccpaChoiceRequest: CCPAChoiceRequest, action: SPActionType): CCPAChoiceResponse =
-        spClient.postChoiceCCPAAction(actionType = action, request = ccpaChoiceRequest)
-    suspend fun postChoice(usNatChoiceRequest: USNatChoiceRequest, action: SPAction): USNatChoiceResponse =
-        spClient.postChoiceUSNatAction(actionType = action.type, request = usNatChoiceRequest)
+    override suspend fun postChoiceCCPA(
+        includeData: IncludeData,
+        authId: String?,
+        action: SPAction
+    ): CCPAChoiceResponse =
+        try {
+            spClient.postChoiceCCPAAction(
+                actionType = action.type,
+                request = CCPAChoiceRequest(
+                    authId = authId,
+                    uuid = state.ccpa?.uuid,
+                    messageId = action.messageId,
+                    pubData = action.encodablePubData,
+                    pmSaveAndExitVariables = action.pmPayload,
+                    sendPVData = state.ccpaMetaData?.wasSampled?: false,
+                    propertyId = propertyId,
+                    sampleRate = state.ccpaMetaData?.sampleRate,
+                    includeData = includeData
+                )
+            )
+        }
+        catch (error: Throwable) {
+            throw error
+        }
+
+    override suspend fun postChoiceUSNat(
+        includeData: IncludeData,
+        idfaStatus:SPIDFAStatus,
+        authId: String?,
+        action: SPAction
+    ): USNatChoiceResponse =
+        try {
+            spClient.postChoiceUSNatAction(
+                actionType = action.type,
+                request = USNatChoiceRequest(
+                    authId = authId,
+                    uuid = state.usNat?.uuid,
+                    messageId = action.messageId,
+                    vendorListId = action.messageId,
+                    pubData = action.encodablePubData,
+                    pmSaveAndExitVariables = action.pmPayload,
+                    sendPVData = state.usNatMetaData?.wasSampled?: false,
+                    propertyId = propertyId,
+                    sampleRate = state.usNatMetaData?.sampleRate,
+                    idfaStatus = idfaStatus,
+                    granularStatus = state.usNat?.consentStatus?.granularStatus,
+                    includeData = includeData
+                )
+            )
+        }
+        catch (error: Throwable) {
+            throw error
+        }
 
 }
 
