58d0a042679005cf87ba210578be8cac72ee3a34
Dmytro <dmytro.f@gowombat.team>
2024-12-19 12:00:11 +0100

    Implement `getChoiceAll`

--- core/src/commonMain/kotlin/com/sourcepoint/mobile_core/Coordinator.kt
+++ core/src/commonMain/kotlin/com/sourcepoint/mobile_core/Coordinator.kt
@@ -1,16 +1,32 @@
 package com.sourcepoint.mobile_core
 
+import com.sourcepoint.mobile_core.models.SPActionType
+import com.sourcepoint.mobile_core.models.consents.GDPRConsent
 import com.sourcepoint.mobile_core.network.SourcepointClient
+import com.sourcepoint.mobile_core.network.requests.CCPAChoiceRequest
+import com.sourcepoint.mobile_core.network.requests.ChoiceAllRequest
+import com.sourcepoint.mobile_core.network.requests.GDPRChoiceRequest
 import com.sourcepoint.mobile_core.network.requests.MetaDataRequest
+import com.sourcepoint.mobile_core.network.requests.USNatChoiceRequest
+import com.sourcepoint.mobile_core.network.responses.CCPAChoiceResponse
+import com.sourcepoint.mobile_core.network.responses.ChoiceAllResponse
+import com.sourcepoint.mobile_core.network.responses.GDPRChoiceResponse
+import com.sourcepoint.mobile_core.network.responses.USNatChoiceResponse
 import com.sourcepoint.mobile_core.storage.Repository
 
+interface SPCoordinator {
+    @Throws(Exception::class) suspend fun getChoiceAll(
+        action: SPActionType,
+        campaigns: ChoiceAllRequest.ChoiceAllCampaigns
+    ): ChoiceAllResponse?
+}
 class Coordinator(
     private val accountId: Int,
     private val propertyId: Int,
     private val propertyName: String,
     private val repository: Repository,
     private val spClient: SourcepointClient
-) {
+): SPCoordinator {
     constructor(accountId: Int, propertyId: Int, propertyName: String) : this(
         accountId,
         propertyId,
@@ -38,4 +54,26 @@ class Coordinator(
         repository.cachedMetaData = metaDataResponse.toString()
         return message
     }
+
+    private fun shouldCallGetChoice(action: SPActionType): Boolean =
+        (action == SPActionType.AcceptAll || action == SPActionType.RejectAll)
+
+    override suspend fun getChoiceAll(action: SPActionType, campaigns: ChoiceAllRequest.ChoiceAllCampaigns): ChoiceAllResponse? =
+        if (shouldCallGetChoice(action))
+            try {
+                spClient.getChoiceAll(action,campaigns)
+            } catch (error: Throwable) {
+                throw error
+            }
+        else
+            null
+
+    suspend fun postChoice(gdprChoiceRequest: GDPRChoiceRequest, action: SPActionType): GDPRChoiceResponse =
+        spClient.postChoiceGDPRAction(actionType = action, request = gdprChoiceRequest)
+
+    suspend fun postChoice(ccpaChoiceRequest: CCPAChoiceRequest, action: SPActionType): CCPAChoiceResponse =
+        spClient.postChoiceCCPAAction(actionType = action, request = ccpaChoiceRequest)
+
+    suspend fun postChoice(usNatChoiceRequest: USNatChoiceRequest, action: SPActionType): USNatChoiceResponse =
+        spClient.postChoiceUSNatAction(actionType = action, request = usNatChoiceRequest)
 }
