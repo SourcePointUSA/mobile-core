b0616fe1057f9fc87183d19d4cbe30bb0a7da14a
Dmytro <dmytro.f@gowombat.team>
2025-01-29 13:17:35 +0100

    Add `initFromCampaign` for `MessageToDisplay`

--- core/src/commonMain/kotlin/com/sourcepoint/mobile_core/models/MessageToDisplay.kt
+++ core/src/commonMain/kotlin/com/sourcepoint/mobile_core/models/MessageToDisplay.kt
@@ -6,6 +6,20 @@ import io.ktor.http.Url
 data class MessageToDisplay(
     val message: MessagesResponse.Message,
     val metaData: MessagesResponse.MessageMetaData,
-    val url: Url,
+    val url: String,
     val type: SPCampaignType
-)
+) {
+    companion object {
+        fun <T> initFromCampaign(campaign: MessagesResponse.Campaign<T>): MessageToDisplay? =
+            if (campaign.message != null && campaign.messageMetaData != null && campaign.url != null) {
+                MessageToDisplay(
+                    message = campaign.message,
+                    metaData = campaign.messageMetaData,
+                    url = campaign.url,
+                    type = campaign.type
+                )
+            } else {
+                null
+            }
+    }
+}
