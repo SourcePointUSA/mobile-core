522157a1a2b0281486a4d50857b9d52a72521d74
Dmytro <dmytro.f@gowombat.team>
2025-01-30 09:59:22 +0100

    Add `updateGDPRStatus` and `updateUSNatStatus` logic for `State`

--- core/src/commonMain/kotlin/com/sourcepoint/mobile_core/models/consents/State.kt
+++ core/src/commonMain/kotlin/com/sourcepoint/mobile_core/models/consents/State.kt
@@ -17,6 +17,12 @@ data class State (
     val hasCCPALocalData: Boolean get() = ccpa?.uuid != null
     val hasUSNatLocalData: Boolean get() = usNat?.uuid != null
 
+    interface SPSampleable {
+        val sampleRate: Float
+        val wasSampled: Boolean?
+        val wasSampledAt: Float?
+    }
+
     data class GDPRMetaData (
         val additionsChangeDate: String,
         val legalBasisChangeDate: String?,
@@ -41,9 +47,34 @@ data class State (
         val applicableSections: List<Int> = emptyList()
     ): SPSampleable
 
-    interface SPSampleable {
-        val sampleRate: Float
-        val wasSampled: Boolean?
-        val wasSampledAt: Float?
+    fun updateGDPRStatus() {
+        if (gdpr == null || gdprMetaData == null)
+            return
+        var shouldUpdateConsentedAll = false
+        if (SPDate(gdpr!!.dateCreated).date < SPDate(gdprMetaData!!.additionsChangeDate).date) {
+            gdpr = gdpr!!.copy(consentStatus = gdpr!!.consentStatus.copy(vendorListAdditions = true))
+            shouldUpdateConsentedAll = true
+        }
+        if (gdprMetaData!!.legalBasisChangeDate != null &&
+            SPDate(gdpr!!.dateCreated).date < SPDate(gdprMetaData!!.legalBasisChangeDate).date) {
+            gdpr = gdpr!!.copy(consentStatus = gdpr!!.consentStatus.copy(legalBasisChanges = true))
+            shouldUpdateConsentedAll = true
+        }
+        if (gdpr!!.consentStatus.consentedAll == true && shouldUpdateConsentedAll) {
+            gdpr = gdpr!!.copy(consentStatus = gdpr!!.consentStatus.copy(granularStatus = gdpr!!.consentStatus.granularStatus?.copy(previousOptInAll = true)))
+            gdpr = gdpr!!.copy(consentStatus = gdpr!!.consentStatus.copy(consentedAll = false))
+        }
+    }
+
+    fun updateUSNatStatus() {
+        if (usNat == null || usNatMetaData == null)
+            return
+        if (SPDate(usNat!!.dateCreated).date < SPDate(usNatMetaData!!.additionsChangeDate).date) {
+            usNat = usNat!!.copy(consentStatus = usNat!!.consentStatus.copy(vendorListAdditions = true))
+            if (usNat!!.consentStatus.consentedAll == true) {
+                usNat = usNat!!.copy(consentStatus = usNat!!.consentStatus.copy(granularStatus = usNat!!.consentStatus.granularStatus?.copy(previousOptInAll = true)))
+                usNat = usNat!!.copy(consentStatus = usNat!!.consentStatus.copy(consentedAll = false))
+            }
+        }
     }
 }
