dedaec499b14274c22e01ce3a08ff78c87ec3704
André Herculano <andresilveirah@gmail.com>
2025-02-21 10:29:33 +0100

    run tests in parallel

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
     kotlin("native.cocoapods") version "2.0.21"
     kotlin("plugin.serialization") version "2.0.21"
     id("com.github.ben-manes.versions") version "0.51.0"
-    id("com.android.library") version "8.7.1"
+    id("com.android.library") version "8.7.3"
     id("com.github.gmazzo.buildconfig") version "5.5.0"
     id("maven-publish")
     id("signing")
@@ -133,6 +133,10 @@ tasks.withType<KotlinNativeSimulatorTest>().configureEach {
     dependsOn("bootIOSSimulator")
     standalone.set(false)
     device.set(deviceName)
+}
+
+tasks.withType<Test> {
+    maxParallelForks = Runtime.getRuntime().availableProcessors()
 }
 
 fun fromProjectOrEnv(key: String): String? = findProperty(key) as String? ?: System.getenv(key)
