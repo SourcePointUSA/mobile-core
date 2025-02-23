4e66bd87a70361ba90fc6fe4a2afcf8e9de43901
André Herculano <andresilveirah@gmail.com>
2025-02-23 11:41:04 +0100

    refactor primitive asserters

--- core/src/commonTest/kotlin/com/sourcepoint/mobile_core/asserters/PrimitiveAsserters.kt
+++ core/src/commonTest/kotlin/com/sourcepoint/mobile_core/asserters/PrimitiveAsserters.kt
@@ -4,54 +4,54 @@ import kotlin.test.assertTrue
 import kotlin.test.assertFalse
 import kotlin.test.fail
 
-fun assertIsEmpty(actual: String?, message: String = "Expected string $actual to be empty") {
-    actual ?: { fail("Expected collection not to be empty, but it's null.") }
-    assertTrue(actual!!.isEmpty(), message)
+fun assertIsEmpty(actual: String?, message: String? = null) {
+    actual ?: { fail(message ?: "Expected collection not to be empty, but it's null.") }
+    assertTrue(actual!!.isEmpty(), message ?: "Expected string $actual to be empty")
 }
 
-fun assertIsEmpty(actual: Collection<*>?, message: String = "Expected collection $actual to be empty") {
-    actual ?: { fail("Expected collection not to be empty, but it's null.") }
-    assertTrue(actual!!.isEmpty(), message)
+fun assertIsEmpty(actual: Collection<*>?, message: String? = null) {
+    actual ?: { fail(message ?: "Expected collection not to be empty, but it's null.") }
+    assertTrue(actual!!.isEmpty(), message ?: "Expected collection $actual to be empty")
 }
 
-fun assertIsEmpty(actual: Map<*, *>?, message: String = "Expected map $actual to be empty") {
-    actual ?: { fail("Expected map not to be empty, but it's null.") }
-    assertTrue(actual!!.isEmpty(), message)
+fun assertIsEmpty(actual: Map<*, *>?, message: String? = null) {
+    actual ?: { fail(message ?: "Expected map not to be empty, but it's null.") }
+    assertTrue(actual!!.isEmpty(), message ?: "Expected map $actual to be empty")
 }
 
-fun assertNotEmpty(actual: String?, message: String = "Expected string $actual not to be empty") {
-    actual ?: { fail("Expected string not to be empty, but it's null.") }
-    assertTrue(actual!!.isNotEmpty(), message)
+fun assertNotEmpty(actual: String?, message: String? = null) {
+    actual ?: { fail(message ?: "Expected string not to be empty, but it's null.") }
+    assertTrue(actual!!.isNotEmpty(), message ?: "Expected string $actual not to be empty")
 }
 
-fun assertNotEmpty(actual: Collection<*>?, message: String = "Expected collection $actual not to be empty") {
-    actual ?: { fail("Expected collection not to be empty, but it's null.") }
-    assertTrue(actual!!.isNotEmpty(), message)
+fun assertNotEmpty(actual: Collection<*>?, message: String? = null) {
+    actual ?: { fail(message ?: "Expected collection not to be empty, but it's null.") }
+    assertTrue(actual!!.isNotEmpty(), message ?: "Expected collection $actual not to be empty")
 }
 
-fun assertNotEmpty(actual: Map<*, *>?, message: String = "Expected map $actual not to be empty") {
-    actual ?: { fail("Expected map not to be empty, but it's null.") }
-    assertTrue(actual!!.isNotEmpty(), message)
+fun assertNotEmpty(actual: Map<*, *>?, message: String? = null) {
+    actual ?: { fail(message ?: "Expected map not to be empty, but it's null.") }
+    assertTrue(actual!!.isNotEmpty(), message ?: "Expected map $actual not to be empty")
 }
 
-fun <T> assertDoesNotContain(iterable: Iterable<T>?, element: T, message: String = "Expected $iterable not to contain $element") {
-    iterable ?: { fail("Expected collection not to contain $element, but it's null.") }
+fun <T> assertDoesNotContain(iterable: Iterable<T>?, element: T, message: String? = null) {
+    iterable ?: { fail(message ?: "Expected collection not to contain $element, but it's null.") }
     iterable!!.firstOrNull { it == element }?.let {
-        fail(message)
+        fail(message ?: "Expected $iterable not to contain $element")
     }
 }
 
-fun <T> assertContains(iterable: Iterable<T>?, element: T, message: String = "Expected $iterable not to contain $element") {
-    iterable ?: { fail("Expected collection to contain $element, but it's null.") }
-    kotlin.test.assertContains(iterable!!, element, message)
+fun <T> assertContains(iterable: Iterable<T>?, element: T, message: String? = null) {
+    iterable ?: { fail(message ?: "Expected collection to contain $element, but it's null.") }
+    kotlin.test.assertContains(iterable!!, element, message ?: "Expected $iterable not to contain $element")
 }
 
-fun assertTrue(actual: Boolean?) {
-    actual ?: { fail("Expected value to be true, but it's null") }
-    assertTrue(actual!!)
+fun assertTrue(actual: Boolean?, message: String? = null) {
+    actual ?: { fail(message ?: "Expected value to be true, but it's null") }
+    assertTrue(actual!!, message)
 }
 
-fun assertFalse(actual: Boolean?) {
-    actual ?: { fail("Expected value to be false, but it's null") }
-    assertFalse(actual!!)
+fun assertFalse(actual: Boolean?, message: String? = null) {
+    actual ?: { fail(message ?: "Expected value to be false, but it's null") }
+    assertFalse(actual!!, message)
 }
