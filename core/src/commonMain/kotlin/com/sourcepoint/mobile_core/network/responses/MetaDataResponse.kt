5ad9243c359ca7fd80298ea35193ee9ea59bb5b6
Dmytro <dmytro.f@gowombat.team>
2025-05-29 11:54:16 +0200

    Add `meta-data` response

--- core/src/commonMain/kotlin/com/sourcepoint/mobile_core/network/responses/MetaDataResponse.kt
+++ core/src/commonMain/kotlin/com/sourcepoint/mobile_core/network/responses/MetaDataResponse.kt
@@ -1,15 +1,14 @@
 package com.sourcepoint.mobile_core.network.responses
 
-import com.sourcepoint.mobile_core.models.consents.IABData
 import com.sourcepoint.mobile_core.models.consents.PreferencesConsent
 import kotlinx.datetime.Instant
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
-import kotlinx.serialization.json.JsonPrimitive
 
 @Serializable
 data class MetaDataResponse (
     val gdpr: MetaDataResponseGDPR? = null,
+    val globalcmp: MetaDataResponseGlobalCmp? = null,
     val usnat: MetaDataResponseUSNat? = null,
     val ccpa: MetaDataResponseCCPA? = null,
     val preferences: MetaDataResponsePreferences? = null
@@ -21,6 +20,15 @@ data class MetaDataResponse (
         val additionsChangeDate: Instant? = null,
         val legalBasisChangeDate: Instant? = null,
         val childPmId: String? = null,
+        @SerialName("_id") val vendorListId: String
+    )
+
+    @Serializable
+    data class MetaDataResponseGlobalCmp (
+        val applies: Boolean,
+        val sampleRate: Float,
+        val additionsChangeDate: Instant? = null,
+        val applicableSections: List<Int>,
         @SerialName("_id") val vendorListId: String
     )
 
